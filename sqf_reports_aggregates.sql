f8038d0a7135ad31c15af55650ebcdefd87ff3c3
Vanessa Hurst <vrhurst@gmail.com>
2011-10-15 18:42:33 -0400

    Separate setup vs loading instructions

---
+++ sqf_reports_aggregates.sql
@@ -0,0 +1,19 @@
+﻿--Precinct Report Quality
+SELECT pct::smallint as precinct
+, COUNT(*)
+FROM sqf_2010
+LEFT JOIN ny_police_precincts npp ON npp.precinct = sqf_2010.pct::smallint
+GROUP BY pct::smallint
+ORDER BY pct::smallint
+
+--Address Data Quality
+SELECT 
+  CASE WHEN COALESCE(TRIM(xcoord),'') <> '' AND COALESCE(TRIM(ycoord),'') <> '' THEN 'Coordinates Present' ELSE 'Coordinates Missing' END as xcoord_present
+, CASE WHEN COALESCE(TRIM(stname),'') <> '' THEN 'Street Present' ELSE 'Street Missing' END as street_name
+, CASE WHEN COALESCE(TRIM(stinter),'') <> '' THEN 'Intersection Present' ELSE 'Intersection Missing' END as intersection
+, COUNT(*)
+FROM sqf_2010
+GROUP BY
+  CASE WHEN COALESCE(TRIM(xcoord),'') <> '' AND COALESCE(TRIM(ycoord),'') <> '' THEN 'Coordinates Present' ELSE 'Coordinates Missing' END
+, CASE WHEN COALESCE(TRIM(stname),'') <> '' THEN 'Street Present' ELSE 'Street Missing' END
+, CASE WHEN COALESCE(TRIM(stinter),'') <> '' THEN 'Intersection Present' ELSE 'Intersection Missing' END
